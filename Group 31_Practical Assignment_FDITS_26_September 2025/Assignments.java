97add9520b04fbc3a6316af050490779cedf7629
Mucyodieudonne <dmucyo720@gmail.com>
2025-09-26 17:07:59 +0200

    Add files via upload
    This is Practical assignment done on 26 September 2025

---
+++ Group 31_Practical Assignment_FDITS_26_September 2025/Assignments.java
@@ -0,0 +1,23 @@
+/**
+=====Group Members(Group number 31)=====
+1)MUCYO Dieudonne 223009625
+2)NDAGIJIMANA Aloyz 223017789
+3)UZABAKIRIHO Theogene 223009518
+ **/
+package com.FDITS;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Assignments extends JPanel implements ActionListener {
+    public Assignments() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
+
---
+++ Group 31_Practical Assignment_FDITS_26_September 2025/Incidents.java
@@ -0,0 +1,23 @@
+/**
+=====Group Members(Group number 31)=====
+1)MUCYO Dieudonne 223009625
+2)NDAGIJIMANA Aloyz 223017789
+3)UZABAKIRIHO Theogene 223009518
+ **/
+package com.FDITS;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class Incidents extends JPanel implements ActionListener {
+    public Incidents() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
